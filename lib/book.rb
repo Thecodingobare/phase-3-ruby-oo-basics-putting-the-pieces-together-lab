87951da2202212798db929c927d8c62d593862b7
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-08-19 01:03:48 +0300

    Completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,81 @@
+class Book
+    attr_reader :title
 
+    def initialize(title)
+        @title = title
+    end
+
+    attr_accessor :author
+    attr_accessor :genre
+    attr_accessor :page_count
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Book
+#     attr_accessor :title, :author, :page_count, :genre
+    
+#     def initialize(title="And Then There Were None",author="Agatha Christie",page_count=272,genre="Mystery")
+#         @title = title
+#         @author = author
+#         @page_count = page_count
+#         @genre=genre
+#     end
+
+#     def turn_page
+#         puts "Flipping the page...wow, you read fast!"
+#     end
+
+
+# end
+
+# book = Book.new()
+# pp book.title
+# pp book.author
+# pp book.genre
+# pp book.page_count
+
+# pp book.turn_page
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,56 @@
+# # Make your shoe class here!
+
+
 # Make your shoe class here!
+class Shoe
+
+    attr_reader :brand
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    attr_accessor :color
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+
+end
+# class Shoe
+#     attr_accessor :brand, :color, :size, :material
+    
+#     def initialize(brand="Nike",color="red",size=9.5,material="suede")
+#         @title = title
+#         @author = author
+#         @page_count = page_count
+#         @genre=genre
+#     end
+
+#     def condition
+#         puts "tattered"
+#     end
+
+#     def cobble
+#         puts "Your shoe is as good as new!"
+#     end
+
+
+# end
+
+# shoe = Shoe.new()
+
+# pp shoe.brand
+# pp shoe.color
+# pp shoe.size
+# pp shoe.material
+
+# pp shoe.condition
+# pp shoe.cobble
+
